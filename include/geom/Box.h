a079ea4068e4222dc3cbc21dd5416b5733913b53
Berkus Decker <berkus@madfire.net>
2017-07-09 14:23:27 +0300

    Refactor Box

--- include/geom/Box.h
+++ include/geom/Box.h
@@ -6,17 +6,34 @@
 #include "math/Vector.h"
 #include "geom/GeomObj.h"
 
-class box : public geometric_object
+class Box : public GeometricObject
 {
     public:
         Vector loc;      // origin
         Vector e1,e2,e3; // edges
 
-        box( Vector&, Vector&, Vector&, Vector& );
-        box( Vector&, double, double, double );
+        Box(Vector const& l, Vector const& s1, Vector const& s2, Vector const& s3)
+            : loc( l )
+            , e1( s1 )
+            , e2( s2 )
+            , e3( s3 )
+        {
+            center = loc + (e1 + e2 + e3) * 0.5;
+            init_normals();
+        }
 
-        virtual bool intersect( ray&, double& );
-        virtual Vector find_normal( Vector& );
+        Box(Vector const& l, double a, double b, double c)
+            : loc( l )
+            , e1( Vector(a,0,0) )
+            , e2( Vector(0,b,0) )
+            , e3( Vector(0,0,c) )
+        {
+            center = loc + (e1 + e2 + e3) * 0.5;
+            init_normals();
+        }
+
+        bool intersect(Ray&, double&) override;
+        Vector find_normal(Vector&) override;
 
     protected:
         Vector n[3];            // normals to sides
