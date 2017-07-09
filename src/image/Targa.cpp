ef718b7bc367218f8a7bf5d3f67b92ee34e49a71
Berkus Decker <berkus@madfire.net>
2017-07-09 19:26:37 +0300

    Cleanup formatting

--- src/image/Targa.cpp
+++ src/image/Targa.cpp
@@ -4,19 +4,20 @@
 #include <string.h>
 #include "image/Targa.h"
 
-void targa_header::save(DataSink& ds) {
-   ds.write1(text_size);
-   ds.write1(map_type);
-   ds.write1(data_type);
-   ds.write2(map_org);
-   ds.write2(map_len);
-   ds.write1(cmap_bits);
-   ds.write2(x_offset);
-   ds.write2(y_offset);
-   ds.write2(width);
-   ds.write2(height);
-   ds.write1(data_bits);
-   ds.write1(im_type);
+void targa_header::save(DataSink& ds)
+{
+    ds.write1(text_size);
+    ds.write1(map_type);
+    ds.write1(data_type);
+    ds.write2(map_org);
+    ds.write2(map_len);
+    ds.write1(cmap_bits);
+    ds.write2(x_offset);
+    ds.write2(y_offset);
+    ds.write2(width);
+    ds.write2(height);
+    ds.write1(data_bits);
+    ds.write1(im_type);
 }
 
 TargaImage::TargaImage( char const*name, int width, int height, char *comment )
@@ -27,41 +28,41 @@ TargaImage::TargaImage( char const*name, int width, int height, char *comment )
     buffer = new rgb [buf_size];
     pos = 0;
 
-   memset(  &hdr, 0, sizeof(hdr) );
-   hdr.data_type =   2;
-   hdr.width = width;
-   hdr.height = height;
-   hdr.data_bits =   24;
-   hdr.im_type = 32;
+    memset(  &hdr, 0, sizeof(hdr) );
+    hdr.data_type =   2;
+    hdr.width = width;
+    hdr.height = height;
+    hdr.data_bits =   24;
+    hdr.im_type = 32;
 
-   if(comment) hdr.text_size = strlen(comment)+1;
+    if(comment) hdr.text_size = strlen(comment)+1;
 
-   hdr.save(*file);
+    hdr.save(*file);
 
-   if(hdr.text_size > 0) file->write(comment, hdr.text_size);
+    if(hdr.text_size > 0) file->write(comment, hdr.text_size);
 }
 
 
 TargaImage::~TargaImage()
 {
-   if(pos > 0) flush();
-   delete buffer;
-   delete file;
+    if(pos > 0) flush();
+    delete buffer;
+    delete file;
 }
 
 
 void TargaImage::put_pixel( rgb color )
 {
-   buffer[pos].r =   color.b; // swap red & blue   colors
-   buffer[pos].g =   color.g;
-   buffer[pos].b =   color.r;
+    buffer[pos].r =   color.b; // swap red & blue   colors
+    buffer[pos].g =   color.g;
+    buffer[pos].b =   color.r;
 
-   if( ++pos >= buf_size ) flush();
+    if( ++pos >= buf_size ) flush();
 }
 
 
 void TargaImage::flush()
 {
-   file->write((char *)buffer, pos * sizeof(rgb));
-   pos = 0;
+    file->write((char *)buffer, pos * sizeof(rgb));
+    pos = 0;
 }
