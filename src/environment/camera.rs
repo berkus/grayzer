349e004d83274dd898ccddb060e44f785e8063e1
Berkus Decker <berkus+github@metta.systems>
2020-04-13 00:19:41 +0300

    Add Camera fov and aspect

--- src/environment/camera.rs
+++ src/environment/camera.rs
@@ -9,12 +9,18 @@ pub struct Camera {
 }
 
 impl Camera {
-    pub fn new_default() -> Self {
+    /// @param vertical_fov Vertical FOV, top to bottom, in degrees.
+    /// @param aspect Width to height aspect ratio.
+    pub fn new(vertical_fov: f32, aspect: f32) -> Self {
+        let theta = vertical_fov.to_radians();
+        let half_height = (theta / 2.0).tan();
+        let half_width = aspect * half_height;
+
         Camera {
             origin: Vec3::new(0.0, 0.0, 0.0),
-            lower_left_corner: Vec3::new(-2.0, -1.0, -1.0),
-            horizontal: Vec3::new(4.0, 0.0, 0.0),
-            vertical: Vec3::new(0.0, 2.0, 0.0),
+            lower_left_corner: Vec3::new(-half_width, -half_height, -1.0),
+            horizontal: Vec3::new(2.0 * half_width, 0.0, 0.0),
+            vertical: Vec3::new(0.0, 2.0 * half_height, 0.0),
         }
     }
 
